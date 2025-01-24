62ef223b9716e9cd827d9d97f07a2188b5bfe167
Biplabini Padhi <biplabini.padhi@gmail.com>
2025-01-24 17:19:47 +0530

    Adding workflows for CI

--- new_func.py
+++ new_func.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 a = 5
 b = 3
 
@@ -11,18 +10,4 @@ def subtract_two_numbers(a, b):
 	return a - b
 
 def multiply_two_numbers(a, b):
-=======
-a = 5
-b = 3
-
-
-def add_two_numbers(a, b):
-	return a + b
-
-
-def subtract_two_numbers(a, b):
-	return a - b
-
-def multiply_two_numbers(a, b):
->>>>>>> fb73bfbee66bb660457e45e5e374b8742aaa1f5e
 	return a * b
